e2043eab8971bbd0f05892fb061731a47e988d72
Shea Kusiak <shkusiak@gmail.com>
2018-06-28 15:36:02 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -87,4 +87,18 @@ class Student
       self.new_from_db(row)
     end
   end
+
+  def self.first_X_students_in_grade_10(x)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 10
+      ORDER BY students.id
+      LIMIT 1;
+    SQL
+
+    DB[:conn].execute(sql, x).map do |row|
+      self.new_from_db(row)
+    end
+  end
 end
